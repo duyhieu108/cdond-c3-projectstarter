07540713c7a0ae104a71c7b9fd7b30867cba31eb
duyhieu108 <duyhieu1008@gmail.com>
2023-07-17 01:30:54 +0800

    update

--- .circleci/ansible/roles/configure-server/files/index.js
+++ .circleci/ansible/roles/configure-server/files/index.js
@@ -1,6 +1,6 @@
-var http = require("http");
-var server = http.createServer(function (req, res) {
-res.writeHead(200);
-res.end("Hello world!");
-});
-server.listen(3030);
+//var http = require("http");
+//var server = http.createServer(function (req, res) {
+//res.writeHead(200);
+//res.end("Hello world!");
+//});
+//server.listen(3030);
